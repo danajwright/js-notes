2def7fded5730ec7f2339d7a269cde860ae6104b
Dana Wright <danajwright@gmail.com>
2016-04-27 17:24:32 -0700

    made relieveDuty function that accepts a vehicle object and a day of the week as parameters

--- codeschool.js
+++ codeschool.js
@@ -611,3 +611,46 @@ lighthouseRock.addBulb = function(name, wattage) {
 
 
 
+
+var vehicle3 = {
+  type: "Submarine", capacity: 8, storedAt: "Underwater Outpost",
+  ranger1: {name: "Gregg Pollack", skillz: "Lasering", dayOff: "Friday"},
+  ranger2: {name: "Bijan Boustani", skillz: "Working", dayOff: "Saturday"},
+  ranger3: {name: "Ashley Smith", skillz: "Torpedoing", dayOff: "Friday"},
+  ranger4: {name: "Mark Krupinski", skillz: "Sniping", dayOff: "Wednesday"},
+  numRangers: 4
+};
+
+var relieveDuty = function(vehicle, day) {
+  var offDuty = [];
+  var onDuty = [];
+
+  for (var i = 1; i <= vehicle.numRangers; i++) {
+    if (vehicle["ranger"+i].dayOff == day) {
+      offDuty.push(vehicle["ranger"+i]);
+    } else {
+      onDuty.push(vehicle["ranger"+i]);
+    }
+    delete vehicle["ranger"+i];
+  }
+
+  vehicle.numRangers -= offDuty.length;
+
+  for (var j = 1; j <= vehicle.numRangers; j++) {
+    vehicle["ranger"+j] = onDuty.shift();
+  }
+
+  return offDuty;
+};
+
+var offToday = relieveDuty(vehicle3, "Friday");
+
+
+
+
+
+
+
+
+
+
